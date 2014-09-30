fddfe043313398e4768adc3808e021af8b4146e8
Michał Budzyński <michal@virtualdesign.pl>
2014-09-30 01:01:00 +0100

    [RANDOM-POLISH-PROVERB] - :point_down: working lib

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict';
 
 var pp = require('polish-proverbs');
+var rpp = function(cb) {
+  pp(function(proverbs) {
+    proverbs = proverbs.split('\n');
 
-pp(function(proverb){
+    cb(proverbs[~~(Math.random()*proverbs.length)]);
+  });
+}
+
+rpp(function(proverb) {
   console.log(proverb);
 });
